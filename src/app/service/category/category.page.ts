ccef3833e1a9e947b2027b11c0900691bbf41019
Ryan <lizkaryan626@gmail.com>
2025-04-12 18:55:23 +0300

    Service filtered

--- src/app/service/category/category.page.ts
+++ src/app/service/category/category.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import {Router} from "@angular/router";
+import { FormsModule } from '@angular/forms';
 
 interface Category {
   id: number;
@@ -14,7 +15,7 @@ interface Category {
   templateUrl: './category.page.html',
   styleUrls: ['./category.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class CategoryPage implements OnInit {
   categories: Category[] = [
@@ -29,6 +30,10 @@ export class CategoryPage implements OnInit {
     // Vous pouvez ajouter d'autres catégories si nécessaire
   ];
 
+  categoriesFiltered: Category[] = this.categories;
+
+  searchName="";
+
   constructor(private router:Router) { }
 
   ngOnInit() {
@@ -38,5 +43,16 @@ export class CategoryPage implements OnInit {
     this.router.navigate(['/service/request'],{
       state: { category: category },
     });
+  }
+
+  filterList($event: any) {
+    if(this.searchName===""){
+      this.categoriesFiltered = [...this.categories];
+    }
+    else {
+      this.categoriesFiltered = this.categories.filter(category => {
+        return category.name.toLowerCase().toLowerCase().includes(this.searchName);
+      })
+    }
   }
 }
